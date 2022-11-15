3d34f8225cbc5d97edeefcd35a4722778319249f
Oh Daehyeon <xzdheogus1@gmail.com>
2022-11-15 20:25:52 +0900

    v1.0.5

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 
 function App() {
 
+  let test = '깃허브 테스트용 텍스트';
+
   let post = '강남 우동 맛집';
   let [글제목, setState] = useState(['남자 코트 추천', '남자 아우터 추천', '남자 미니멀 패션']);
   let [good, goodchage] = useState(0);
